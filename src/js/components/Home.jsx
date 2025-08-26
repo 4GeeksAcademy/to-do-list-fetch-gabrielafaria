d27492be39e710f977906756679f3cccaab01d91
gabrielafariac <126210298+gabrielafariac@users.noreply.github.com>
2025-08-26 01:25:58 +0000

    list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const url = "https://playground.4geeks.com/todo";
 
-//create your first component
+const createUser = () => {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: "gabrielaFaria", id: 0 }),
+  };
+  fetch(url + "/users/gabrielaFaria", options)
+    .then((r) => r.json())
+    .then((d) => console.log("created user data:", d));
+};
+
+const getAllUsers = () => {
+  fetch(url + "/users")
+    .then((resp) => resp.json())
+    .then((data) => console.log("getAllUsersData:", data));
+};
+
+const updateToDo = () => {
+  let options = {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      label: "string",
+      is_done: false,
+    }),
+  };
+  fetch(url + "/todos/36", options)
+    .then((resp) => resp.json())
+    .then((data) => console.log("updated:", data));
+};
+
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [chores, setChores] = useState([]);
+  const [newchore, setNewChore] = useState({ label: "", is_done: false });
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const nextChore = () => {
+    setChores([newchore, ...chores]);
+    setNewChore({ label: "", is_done: false });
+  };
+
+  const deleteToDo = (ID) => {
+    let options = { method: "DELETE" };
+    fetch(url + "/todos/" + ID, options)
+      .then((resp) => resp.json())
+      .then((data) => console.log("deleted :", data));
+	  getTodos();
+	
+  };
+
+  const getTodos = () => {
+    fetch(url + "/users/gabrielaFaria")
+      .then((resp) => resp.json())
+      .then((data) => setChores(data.todos));
+  };
+
+  const addToDo = (Label) => {
+    let options = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        label: Label,
+        is_done: false,
+      }),
+    };
+    fetch(url + "/todos/gabrielaFaria", options)
+      .then((resp) => resp.json())
+      .then((data) => console.log("dataAddToDos:", data));
+    getTodos();
+  };
+
+  useEffect(() => {
+    createUser();
+    getTodos();
+  }, []);
+
+  return (
+    <div className="container text-center">
+      <h1 >To Do List</h1>
+
+      <input
+        value={newchore.label}
+        type="text"
+        placeholder="Add new task"
+        onChange={(e) =>
+          setNewChore({ label: e.target.value, is_done: false })
+        }
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            nextChore();
+            addToDo(newchore.label);
+          }
+        }}
+      />
+
+      <ul>
+        {chores.map((item, i) => (
+          <div  className="choresList" key={i + "chores"}>
+            <li>{item.label}</li>
+            <span
+              style={{ cursor: "pointer", marginLeft: "8px", color: "red" }}
+              onClick={() => deleteToDo(item.id)}
+            >
+              X
+            </span>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
